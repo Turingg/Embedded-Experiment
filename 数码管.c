02b8a8eb2762fb0742c0801d8adc7c829988d136
Turingggg <732153168@qq.com>
2025-07-25 11:59:16 +0800

    first commit

---
+++ 数码管.c
@@ -0,0 +1,244 @@
+#include "config.h"
+#include "LPC2200.h"
+
+#define SI_BIT (1<<18)
+#define RCK_BIT (1<<20)
+#define SCK_BIT (1<<17)
+#define LED 16
+
+#define SI_KEY (1<<6)
+#define RCK_KEY (1<<7)
+#define SCK_KEY (1<<4)
+#define KEY_IN (1<<5)//P0.5
+
+typedef unsigned short U16;
+typedef unsigned char U8;
+typedef unsigned int U32;
+U8 ii=0;
+U8 jj=0;
+U8 key_scan_bit = 0; 
+U8 display_buffer[8]; // 显示缓冲区
+
+const U8 code[LED]=
+{
+    //0x3F,0x06,0x5b,0x4F,0x66,0x6D,0x7D,0x07,
+    //0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71
+    //data & (0x800O);
+    //data = (data<<1);
+
+    0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0,
+    0xFE, 0xE6, 0xEE, 0x3E, 0x9C, 0x7A, 0x9E, 0x8E
+    //data & (0x00O1);
+    //data = (data>>1);
+};
+
+void Init(void)
+{
+    // P0.18
+    PINSEL1 &= ~(1<<4);
+    PINSEL1 &= ~(1<<5);
+    IO0DIR |= SI_BIT;
+
+    // P0.20
+    PINSEL1 &= ~(1<<8);
+    PINSEL1 &= ~(1<<9);
+    IO0DIR |= RCK_BIT;
+
+    // P0.17
+    PINSEL1 &= ~(1<<2);
+    PINSEL1 &= ~(1<<3);
+    IO0DIR |= SCK_BIT;
+}
+
+void Init_key()
+{
+    // P0.4
+    PINSEL0 &= ~(1<<8);
+    PINSEL0 &= ~(1<<9);
+    IO0DIR |= SCK_KEY;
+
+    // P0.7
+    PINSEL0 &= ~(1<<14);
+    PINSEL0 &= ~(1<<15);
+    IO0DIR |= RCK_KEY;
+
+    // P0.6
+    PINSEL0 &= ~(1<<12);
+    PINSEL0 &= ~(1<<13);
+    IO0DIR |= SI_KEY;
+
+    // P0.5 GPIO输入(0)功能，检测按键
+    PINSEL0 &= ~(1<<10);
+    PINSEL0 &= ~(1<<11);
+    IO0DIR &= ~KEY_IN;
+    //置0，设置为输入
+    //IO0DIR |= KEY_IN;
+    ////置1，设置为输出
+}
+
+void delay(U32 num)
+{
+    U32 j;
+    for(; num>0; num--)
+    {
+        for(j=0; j<500; j++)
+        {
+             //delay(1) 延时非常短，适合扫描
+        }
+    }
+}
+
+void Send_HalfWord(U8 select, U8 data)
+{
+    U8 i;
+    // RCK拉低，准备数据
+    IO0CLR = RCK_BIT;//写 '1' 清零，写 '0' 无效
+
+    // 低八位的片选
+    for(i=0; i<8; i++)
+    {
+        IO0CLR = SCK_BIT;
+        if(select & (0x0001))
+        {
+            // SI为1
+            IO0SET = SI_BIT;
+        }
+        else
+        {
+            // SI为0
+            IO0CLR = SI_BIT;
+        }
+        // SCK拉高，传输数据
+        IO0SET = SCK_BIT;
+        // 数据右移
+        select = (select>>1);
+        IO0CLR = SCK_BIT;
+    }
+
+    // 高八位的段码
+    for(i=0; i<8; i++)
+    {
+        IO0CLR = SCK_BIT;
+        if(data & (0x0001))
+        {
+            // SI为1
+            IO0SET = SI_BIT;
+        }
+        else
+        {
+            // SI为0
+            IO0CLR = SI_BIT;
+        }
+        // SCK拉高，传输数据
+        IO0SET = SCK_BIT;
+        // 数据右移
+        data = (data>>1);
+        IO0CLR = SCK_BIT;
+    }
+    
+    // RCK拉高，锁存并输出数据
+    IO0SET = RCK_BIT;
+}
+
+//键盘发送函数
+void SendKEY_Word(U16 data)
+{
+    U8 i=0;
+    // RCK拉低，准备数据 
+    IO0CLR = RCK_KEY;
+    for(i=0; i<16; i++)
+    {
+        // SCK拉低，准备数据
+        IO0CLR = SCK_KEY;
+        //if(data & 0x0001)
+        if(data & 0x8000)
+        {
+            IO0SET = SI_KEY;
+        }
+        else
+        {
+            IO0CLR = SI_KEY;
+        }
+        IO0SET = SCK_KEY;
+        //data >>= 1;
+        data <<= 1;
+        
+    }
+    IO0CLR = SCK_KEY;
+    // RCK拉高，锁存并输出数据
+    IO0SET = RCK_KEY;
+}
+
+U8 Scan_KEY(void)   
+{
+
+    U16 select_word; 
+    //低电平有效
+    select_word = 0xFFFF & (~(1 << key_scan_bit));
+
+    SendKEY_Word(select_word);
+     
+    if(0 == (IO0PIN & KEY_IN))
+    {
+
+        U8 pressed_key = key_scan_bit; 
+        key_scan_bit++;
+        if (key_scan_bit == 16) 
+        {
+        	key_scan_bit = 0;    
+        }
+        return pressed_key; 
+    }
+    key_scan_bit++;
+    if (key_scan_bit == 16) 
+    {
+    	key_scan_bit = 0;
+    }
+    return 0xFF; 
+}
+
+int main(void)
+{
+    U8 ret_key = 0xFF;    
+    U8 i;                 
+
+    Init();
+    Init_key();
+
+     
+    for(i = 0; i < 8; i++) 
+    {
+    	display_buffer[i] = 0x00;
+    }
+    
+    while(1)
+    {
+        
+        ret_key = Scan_KEY(); 
+        if (ret_key != 0xFF) 
+        {  
+          
+	        for(i = 0; i < 8; i++) 
+	   		{
+	        	display_buffer[i] = code[ret_key];
+	    	}  
+        } 
+        else 
+        {   
+          
+            for(i = 0; i < 8; i++) 
+            {
+                display_buffer[i] = 0x00;
+            }
+        }
+        
+        Send_HalfWord(0xFF & (~(1 << jj)), display_buffer[jj]);
+  
+        jj++;
+        if(jj == 8)
+        {
+            jj = 0;
+        }
+    }
+    return 0;
+}
---
+++ 贪吃蛇.c
@@ -0,0 +1,435 @@
+﻿#include "LPC2200.h"
+#include "config.h"
+// 点阵屏控制引脚定义：使用 P0.17 ~ P0.20
+#define LATIC_SCK  (1 << 17)  // SCK: 串行时钟
+#define LATIC_SI_X (1 << 18)  // SI_X: 行数据（蛇身图案）
+#define LATIC_SI_Y (1 << 19)  // SI_Y: 列地址（扫描哪一行）
+#define LATIC_RCK  (1 << 20)  // RCK: 锁存时钟
+#define SPEAKER_PIN (1 << 24)
+volatile uint8 game_running = 0;
+uint32 matrix[16] = {0};
+uint32 score = 0;
+uint32 key_scan_bit = 0;
+// 数码管显示编码
+const uint8 display_code[16] = 
+{
+    0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0,
+    0xFE, 0xE6, 0xEE, 0x3E, 0x9C, 0x7A, 0x9E, 0x8E
+};
+
+// 移位寄存器控制引脚初始化
+void Led_Init() 
+{
+    // 初始化SI引脚
+    PINSEL1 &= ~(1 << 4);     // P0.18作为GPIO
+    PINSEL1 &= ~(1 << 5);
+    IO0DIR |= (1<<18);
+
+    // 初始化RCK引脚
+    PINSEL1 &= ~(1 << 8);     // P0.20作为GPIO
+    PINSEL1 &= ~(1 << 9);
+    IO0DIR |= (1<<20);
+
+    // 初始化SCK引脚
+    PINSEL1 &= ~(1 << 2);     // P0.17作为GPIO
+    PINSEL1 &= ~(1 << 3);
+    IO0DIR |= (1<<17);
+}
+
+// 发送半字节数据到移位寄存器
+void Send_HalfWord(uint8 pos, uint8 data) 
+{
+    uint8 i;
+    IO0CLR = (1<<20);        // 准备锁存数据
+
+    // 发送位置信息（8位）
+    for (i = 0; i < 8; i++) 
+    {
+        IO0CLR = (1<<17);
+        if (pos & 0x01) 
+        {
+            IO0SET = (1<<18);
+        } 
+        else 
+        {
+            IO0CLR = (1<<18);
+        }
+        IO0SET = (1<<17);    // 上升沿移位
+        pos >>= 1;
+    }
+
+    // 发送数据信息（8位）
+    for (i = 0; i < 8; i++) 
+    {
+        IO0CLR = (1<<17);
+        if (data & 0x01) 
+        {
+            IO0SET = (1<<18);
+        } 
+        else 
+        {
+            IO0CLR = (1<<18);
+        }
+        IO0SET = (1<<17);    // 上升沿移位
+        data >>= 1;
+    }
+    IO0SET = (1<<20);        // 锁存输出数据
+}
+
+void delay(uint32 t)
+{
+    uint32 i,j;
+    for(i=0;i<t;i++)
+    {
+        for(j=0;j<100000;j++)
+        {
+        }
+    }
+}
+void Beep_on()
+{
+	IO0CLR = (1<<24);
+}
+void Beep_off()
+{
+	IO0SET = (1<<24);
+}
+void Beep_init(void)
+{
+	PINSEL1 &= ~(1<<16);
+	PINSEL1 &= ~(1<<17);
+	IO0DIR |= SPEAKER_PIN;
+	Beep_off();
+}
+
+void gpio_init(void)
+{
+    // 设置 P0.17 ~ P0.20 为 GPIO
+    PINSEL1 &= 0xFFFFFC03;
+    IO0DIR |= (LATIC_SCK | LATIC_RCK | LATIC_SI_X | LATIC_SI_Y);
+}
+void latic_send(uint32 row, uint32 col)
+{
+    uint32 i;
+    IO0CLR = LATIC_RCK;
+    for (i = 0; i < 16; i++)
+    {
+        IO0CLR = LATIC_SCK;
+
+        // 发送行数据
+        if (row & 0x8000)
+        {
+            IO0SET = LATIC_SI_X;
+        }
+        else
+        {
+            IO0CLR = LATIC_SI_X;
+        }
+
+        // 发送列数据
+        if (col & 0x8000)
+        {
+            IO0SET = LATIC_SI_Y;
+        }
+        else
+        {
+            IO0CLR = LATIC_SI_Y;
+        }
+
+        IO0SET = LATIC_SCK;  // 上升沿送数据
+        row <<= 1;
+        col <<= 1;
+    }
+    IO0SET = LATIC_RCK;  // 锁存输出
+}
+
+void display_frame(uint32 *frame)
+{
+    uint32 i;
+    for (i = 0; i < 16; i++)
+    {
+        latic_send(frame[i], ~(1 << i));
+    }
+}
+
+void clear_display(void)
+{
+    uint32 i;
+    for (i = 0; i < 16; i++)
+    {
+        matrix[i] = 0x0000;
+    }
+    display_frame(matrix);
+}
+
+void init_keyboard_gpio(void)
+{
+
+    // P0.4
+    PINSEL0 &= ~(1<<8);
+    PINSEL0 &= ~(1<<9);
+
+    // P0.7
+    PINSEL0 &= ~(1<<14);
+    PINSEL0 &= ~(1<<15);
+
+    // P0.6
+    PINSEL0 &= ~(1<<12);
+    PINSEL0 &= ~(1<<13);
+
+    // P0.5 GPIO输入(0)功能，检测按键
+    PINSEL0 &= ~(1<<10);
+    PINSEL0 &= ~(1<<11);
+
+    IO0DIR |= (1 << 6) | (1 << 7) | (1 << 4);
+    IO0DIR &= ~(1 << 5);
+}
+
+//键盘发送函数，按下键盘时扫描对应键码的键盘
+void send_keyboard_word(uint32 data)
+{
+    uint32 i;
+    IO0CLR = (1 << 7);
+    for (i = 0; i < 16; i++)
+    {
+        IO0CLR = (1 << 4);
+        if (data & 0x8000)
+        {
+            IO0SET = (1 << 6);
+        }
+        else
+        {
+            IO0CLR = (1 << 6);
+        }
+        IO0SET = (1 << 4);
+        data <<= 1;
+    }
+    IO0SET = (1 << 7);
+}
+
+uint32 read_direction(void)
+{
+
+    uint32 select_word = ~(1 << key_scan_bit);
+    //发送键码，选通对应的键盘，扫描对应位
+    send_keyboard_word(select_word);
+
+    if (!(IO0PIN & (1 << 5)))
+    {
+    	uint32 pressed_key = key_scan_bit;
+        //有按键按下
+        key_scan_bit = (key_scan_bit + 1) % 16;
+        switch (pressed_key)
+        {
+            case 10:
+                return 10; // 下
+            case 11:
+                return 11; // 上
+            case 14:
+                return 14; // 左
+            case 15:
+                return 15; // 右
+            default:
+                return 255;
+        }
+    }
+    else
+    {
+        //无按键按下
+        key_scan_bit = (key_scan_bit + 1) % 16;
+        return 255;
+    }
+}
+
+// ---- Snake Logic ----
+
+#define MAX_LENGTH 64
+uint32 snake_x[MAX_LENGTH], snake_y[MAX_LENGTH];
+uint32 length = 3;
+uint32 dir = 15; // 初始向右
+uint32 food_x, food_y;
+
+void init_snake(void)
+{
+	uint32 i;
+    length = 3;
+    for (i = 0; i < length; i++)
+    {
+        snake_x[i] = 5 - i;
+        snake_y[i] = 5;
+    }
+    dir = 15;
+}
+
+void generate_food(void)
+{
+    //食物位置，通过定时器0伪随机生成
+    //T0TC，定时计数器0的TC值
+    food_x = 1 + (T0TC % 14);
+    food_y = 1 + (T0TC / 14 % 14);
+}
+
+void set_direction(uint8 d)
+{
+    if ((dir == 10 && d != 11) || (dir == 11 && d != 10) ||(dir == 14 && d != 15) || (dir == 15 && d != 14))
+    {
+        dir = d;
+    }
+}
+
+uint8 update_snake(void)
+{
+	uint32 i;
+    //蛇头的新位置
+    uint32 new_x = snake_x[0];
+    uint32 new_y = snake_y[0];
+    if (dir == 10)
+    {
+        new_y--;
+    }
+    else if (dir == 11)
+    {
+        new_y++;
+    }
+    else if (dir == 14)
+    {
+        new_x--;
+    }
+    else if (dir == 15)
+    {
+        new_x++;
+    }
+    //检查是否撞到边界
+    if (new_x > 15 || new_y > 15 || new_x < 0 || new_y <0 )
+    {
+        return 0;
+    }
+    //检查是否撞到自身
+    for (i = 0; i < length; i++)
+    {
+        if (snake_x[i] == new_x && snake_y[i] == new_y)
+        {
+            return 0;
+        }
+    }
+    matrix[snake_y[length-1]] &= ~(1<<(snake_x[length - 1]));
+    //更新蛇的位置
+    for (i = length; i > 0; i--)
+    {
+        snake_x[i] = snake_x[i - 1];
+        snake_y[i] = snake_y[i - 1];
+    }
+    snake_x[0] = new_x;
+    snake_y[0] = new_y;
+    if (new_x == food_x && new_y == food_y)
+    {
+    	Beep_on();
+        if (length < MAX_LENGTH - 1)
+        {
+            length++;
+        }
+        score++;
+        delay(10);
+        Beep_off();
+        matrix[food_y] &= ~(1<<(food_x));
+        generate_food();
+    }
+    return 1;
+}
+
+//渲染蛇和食物的位置，以及分数
+void render_snake(void)
+{
+    uint32 i;
+    for (i = 0; i < length; i++)
+    {
+        matrix[snake_y[i]] |= (1 << (snake_x[i]));
+    }
+    matrix[food_y] |= (1 << (food_x));
+    //显示具体实现应由数码管实现
+}
+
+
+void __irq TIMER0_IRQHandler(void)
+{
+    uint32 i,j,k,f;
+    Send_HalfWord(0xFF,display_code[score]);
+    if (game_running)
+    {
+        f=update_snake();
+        if (f)
+        {
+        	render_snake();
+            for(i=0;i<2000;i++)
+            {
+                display_frame(matrix);
+            }
+            
+        }
+        else
+        {
+            //游戏结束
+            game_running = 0;
+            //闪烁显示当前状态，提示游戏结束
+            for (i = 0; i < 3; i++)
+            {
+            	Beep_on();
+                for (j = 0; j < 700; j++)
+                {
+                    for(k=0;k<16;k++)
+                    {
+                         latic_send(0x0000, ~(1 << j));
+                    }
+                }
+                Beep_off();
+                for (j = 0; j < 700; j++)
+                {
+                   display_frame(matrix);
+                }
+            }
+        }
+    }
+    //清除定时器0中断标志，写“1”有效，复位清除中断；写“0”无效
+    T0IR = 1;
+    VICVectAddr = 0;
+}
+
+void init_timer0(void)
+{
+    //预分频
+    T0PR = 0;
+    T0MR0 = Fpclk/1000;
+    //产生中断，TC复位
+    T0MCR = 3;
+    //置0，定时器0IRQ
+    VICIntSelect &= ~(1 << 4);
+    VICVectCntl0 = (1 << 5) | 4;
+    VICVectAddr0 = (uint32)TIMER0_IRQHandler;
+    //使能中断
+    VICIntEnable = 1 << 4;
+    //使能定时器0
+    T0TCR = 1;
+}
+
+int main(void)
+{
+	uint32 i;
+    gpio_init();
+    Beep_init();
+    Led_Init();
+    clear_display();
+    init_snake();
+    init_timer0();
+    generate_food();
+    init_keyboard_gpio();
+    game_running = 1;
+	
+    while (1)
+    {
+        uint8 dir = read_direction();
+        if (dir != 255)
+        {
+            set_direction(dir);
+        }
+    }
+}
